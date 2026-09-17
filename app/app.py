e28ef7d2fbd153e46020b9b00c6dedac3bad24a8
Mike Cayan <mahirturan9097@gmail.com>
2026-09-17 12:32:02 +0200

    feat: add containerized flask application

---
+++ app/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "Secure Azure Container Platform is running"
+
+
+@app.route("/health")
+def health():
+    return {"status": "ok"}, 200
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
